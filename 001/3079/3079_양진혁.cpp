5a9e5c0e80e91a267fa864bb59b15d3215827f41
zNyuk96 <alxn8329@gmail.com>
2021-07-27 10:28:20 +0900

    양진혁210727

---
+++ 001/3079/3079_양진혁.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+long long arr[100000] = { 0, };
+int N, M;
+
+long long bs(long long s, long long e) {
+    if (s == e) return s;
+    long long m = s + e >> 1;
+    long long count = 0;
+    for (int i = 0; i < N; i++) {
+        count += m / arr[i];
+    }
+    if (count >= M) {
+        return bs(s, m);
+    }
+    else {
+        return bs(m+1, e);
+    }
+}
+int main()
+{
+    std::ios_base::sync_with_stdio(0);
+    std::cin.tie(0);
+    std::cin >> N >> M;
+    long long TMAX = 0, temp;
+    for (int i = 0; i < N; i++) {
+        std::cin >> temp;
+        TMAX = (temp > TMAX) ? temp : TMAX;
+        arr[i] = temp;
+    }
+    long long MMAX = TMAX * M;
+    long long res = bs(0, MMAX);
+    std::cout << res;
+}
