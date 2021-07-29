a7019bbde976066b29a44ddf24ad9e7172999ff5
jiwon oh <tinwoon21@gmail.com>
2021-07-29 21:18:19 +0900

    jiwon 문제 풀이 소스 확인

---
+++ 001/3020/3020_오지원.cpp
@@ -0,0 +1,40 @@
+﻿#pragma warning (disable:4996)
+#include <iostream>
+#include <vector>
+#include <limits.h>
+#include <algorithm>
+
+std::vector<int> top;
+std::vector<int> bottom;
+int N, H;
+
+std::pair<int, int> calculate() {
+	int ans = INT_MAX;
+	int count = 0;
+	for (int k = 1; k <= H; k++) {
+		int bottom_value = std::distance(std::lower_bound(bottom.begin(), bottom.end(), k), bottom.end());
+		int top_value = std::distance(std::lower_bound(top.begin(), top.end(), H - k + 1), top.end());
+		if (ans == bottom_value + top_value) count++;
+		else if (ans > bottom_value + top_value) {
+			ans = bottom_value + top_value;
+			count = 1;
+		}
+	}
+	return std::make_pair(ans, count);
+}
+
+int main() {
+	scanf("%d %d", &N, &H);
+	for (int k = 0; k < N; k++) {
+		int value;
+		scanf("%d", &value);
+		if (k % 2 == 0) bottom.emplace_back(value);
+		else top.emplace_back(value);
+	}
+	std::sort(top.begin(), top.end());
+	std::sort(bottom.begin(), bottom.end());
+	auto rst = calculate();
+
+	printf("%d %d", rst.first, rst.second);
+
+}
---
+++ 001/3079/3079_오지원.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <limits.h>
+
+long long N, M;
+std::vector<long long> entrance;
+
+long long calculate() {
+	long long begin = 0;
+	long long end = entrance[N - 1] * M;
+
+	long long ans = LLONG_MAX;
+
+	while (begin <= end) {
+		long long mid = (begin + end) / 2;
+
+		long long sum = 0;
+		for (int k = 0; k < N; k++) sum += mid / entrance[k];
+
+		if (sum >= M) {
+			ans = std::min(ans, mid);
+			end = mid - 1;
+		}
+		else {
+			begin = mid + 1;
+		}
+	}
+	return ans;
+}
+
+int main() {
+	scanf("%d %d", &N, &M);
+	entrance.assign(N, 0);
+	for (int k = 0; k < N; k++) {
+		scanf("%d", &entrance[k]);
+	}
+	std::sort(entrance.begin(), entrance.end());
+	printf("%lli", calculate());
+}
