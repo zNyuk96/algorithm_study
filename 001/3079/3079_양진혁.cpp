2c1782aa5203eac387e9bac1f3e4ab0138ebf64b
jiwon oh <tinwoon21@gmail.com>
2021-07-29 21:33:02 +0900

    진혁님 코드 원래대로 돌려놓기 feat.jiwon

--- 001/3079/3079_양진혁.cpp
+++ 001/3079/3079_양진혁.cpp
@@ -1,52 +1,34 @@
 ﻿#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <tuple>
-#include <math.h>
-#include <limits.h>
 
-int N;
-std::vector<long long> value;
-std::vector<long long> ans(3, 0);
-long long ans_val = LLONG_MAX;
+long long arr[100000] = { 0, };
+int N, M;
 
-void check_ans(long long two_sum, int three_index, int i, int j) {
-	if (llabs(two_sum + value[three_index]) < ans_val) {
-		ans[0] = value[i];
-		ans[1] = value[j];
-		ans[2] = value[three_index];
-		ans_val = llabs(two_sum + value[three_index]);
-	}
+long long bs(long long s, long long e) {
+    if (s == e) return s;
+    long long m = s + e >> 1;
+    long long count = 0;
+    for (int i = 0; i < N; i++) {
+        count += m / arr[i];
+    }
+    if (count >= M) {
+        return bs(s, m);
+    }
+    else {
+        return bs(m + 1, e);
+    }
 }
-
-void calculate() {
-
-	for (int i = 0; i < N; i++) {
-		for (int j = i + 1; j < N; j++) {
-			long long two_sum = value[i] + value[j];
-			//이거 앞뒤 전후에 최적 값이 들어가 있음.
-			int three_index = std::distance(value.begin(), std::lower_bound(value.begin(), value.end(), two_sum * -1));
-			if (three_index < N && i != three_index && j != three_index) {
-				check_ans(two_sum, three_index,i,j);
-			}
-			if (three_index > 0 && i != three_index - 1 && j != three_index - 1) {
-				check_ans(two_sum, three_index - 1,i,j);
-			}
-		}
-	}
-	std::sort(ans.begin(), ans.end());
-	return;
-}
-
-int main() {
-	scanf("%d", &N);
-	value.assign(N, 0);
-	for (int k = 0; k < N; k++) {
-		scanf("%lld", &value[k]);
-	}
-	std::sort(value.begin(), value.end());
-
-	calculate();
-	printf("%lld %lld %lld", ans[0], ans[1], ans[2]);
-
+int main()
+{
+    std::ios_base::sync_with_stdio(0);
+    std::cin.tie(0);
+    std::cin >> N >> M;
+    long long TMAX = 0, temp;
+    for (int i = 0; i < N; i++) {
+        std::cin >> temp;
+        TMAX = (temp > TMAX) ? temp : TMAX;
+        arr[i] = temp;
+    }
+    long long MMAX = TMAX * M;
+    long long res = bs(0, MMAX);
+    std::cout << res;
 }
