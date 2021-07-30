326617f63092eb7153ec597cb2c7c976f018d79e
김형준 <ninini6630@gmail.com>
2021-07-30 10:34:46 +0900

    자료형 확장 + 재귀조건 일부 수정

--- 001/3079/3079_김형준.cpp
+++ 001/3079/3079_김형준.cpp
@@ -2,15 +2,15 @@
 
 using namespace std;
 
-int M, N;
-int T[100000];
+long long M, N; 
+long long T[100000];
 
-int BSearch(int low, int high)
+long long BSearch(long long low, long long high)
 {
     if (low >= high) return low;
 
-    int mid = (low + high) / 2;
-    int people = 0;
+    long long mid = (low + high) / 2;
+    long long people = 0;
 
     for (int i = 0; i < N; i++)
     {
@@ -19,7 +19,7 @@ int BSearch(int low, int high)
     }
 
     if (people >= M)
-        return BSearch(low, mid-1);
+        return BSearch(low, mid); // >=조건에서 재귀 high가 mid-1이었던 부분 수정
     else
         return BSearch(mid+1, high);
 }
@@ -27,7 +27,7 @@ int BSearch(int low, int high)
 int main()
 {
     // int max = 0;
-    int min = 1000000001;
+    long long min = 1000000001;
     
     // cin cout 속도 저하 해결용
     ios::sync_with_stdio(false);
@@ -35,7 +35,7 @@ int main()
 
     cin >> N >> M;
 
-    for (int i = 0; i < N; i++)
+    for (long long i = 0; i < N; i++)
     {
         cin >> T[i];
         // if (max < T[i]) max = T[i];
