41e5cfc3322af8886894d1d1150e795e2e67555e
sunminky <suns1502@gmail.com>
2021-08-12 22:57:06 +0900

    선민기 3주차

--- 005/3151/3151_선민기.cpp
+++ 005/3151/3151_선민기.cpp
@@ -21,14 +21,12 @@ int main() {
 	for (int i = 0; i < n - 1; i++) {
 		for (int j = i + 1; j < n; j++) {
 			diff = -(member[i] + member[j]);
-			high = upper_bound(member.begin() + i + 1, member.end() + j, diff) - member.begin();
+			high = upper_bound(member.begin() + i + 1, member.begin() + j, diff) - member.begin();
 			low = lower_bound(member.begin() + i + 1, member.begin() + high, diff) - member.begin();
 			answer += high - low;
 		}
 	}
 	cout << answer << endl;
-
-	cout << lower_bound(member.begin() + 3, member.begin() + 3, diff) - member.begin() << endl;
 
 	return 0;
 }
